d821bbc84591635dec69d0cf310bd6acae65f2f6
tawsif-r <urrahmantawsif@gmail.com>
2025-10-21 12:11:44 +0600

    added docker compose and containerized the whole project

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,21 @@
+# Stage 1: Base runtime
 FROM mcr.microsoft.com/dotnet/aspnet:9.0 AS base
 WORKDIR /app
 EXPOSE 8080
+ENV ASPNETCORE_URLS=http://+:8080
 
+# Stage 2: Build
 FROM mcr.microsoft.com/dotnet/sdk:9.0 AS build
 WORKDIR /src
 COPY . .
-RUN dotnet restore
-RUN dotnet build -c Release -o /app/build
+RUN dotnet restore "ecommerce.csproj"
+RUN dotnet build "ecommerce.csproj" -c Release -o /app/build
 
+# Stage 3: Publish
 FROM build AS publish
-RUN dotnet publish -c Release -o /app/publish
+RUN dotnet publish "ecommerce.csproj" -c Release -o /app/publish /p:UseAppHost=false
 
+# Stage 4: Final image
 FROM base AS final
 WORKDIR /app
 COPY --from=publish /app/publish .
--- obj/Debug/net9.0/ecommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a30852d2db17337930c8db2aaa9a4cdc23f877b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9b64d9ef8c487768c0e7be9fa71ea19cdbaa4cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
