7ff2be9d4a98c0a1ad3e9463ddc7d08e7f43bb5c
tawsif-r <urrahmantawsif@gmail.com>
2025-10-31 18:53:19 +0600

    added nothing

--- obj/Debug/net9.0/ecommerce.AssemblyInfo.cs
+++ obj/Debug/net9.0/ecommerce.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a19a5b0bcec9f1266754868fb802f4943e169352")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e881466b4a15afaad2038cb3708a4e50690e8e91")]
 [assembly: System.Reflection.AssemblyProductAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ecommerce")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
